c525b087b974c08e0d214956e4bb7dc1eec2ba47
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-22 00:19:39 +0200

    fixing output

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,28 +4,23 @@ For a given employee ID, returns information about his/her TODO list progress.
 '''
 
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    
-    USER_ID = sys.argv[1]
     user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                             format(USER_ID)).json()
-    
-    EMPLOYEE_NAME = user_info.get("username")
-    user_tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                              format(USER_ID)).json()
+                             format(argv[1])).json()
+
+    user_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(argv[1])).json()
+    titles = []
+
     for tasks in user_tasks:
         if tasks.get('completed') is True:
-            TASK_TITLE.append(tasks.get('title'))
-            NUMBER_OF_DONE_TASKS += 1
-    TOTAL_NUMBER_OF_DONE_TASKS = len(user_tasks)
+            titles.append(tasks.get('title'))
 
     print('Employee {} is done with tasks({}/{}):'.
-          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, 
-                 TOTAL_NUMBER_OF_DONE_TASKS))
-    for title in TASK_TITLE:
-        print('\t {}'.format(title))
+          format(user_info.get('name'), len(titles), len(user_info))),
+
+    print('\n'.join('\t {}'.format(task) for task in titles))
