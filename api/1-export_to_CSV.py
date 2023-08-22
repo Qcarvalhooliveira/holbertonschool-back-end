30ec819d9bc1749e3c598ae18ac7ff181fa332b4
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-22 18:46:44 +0200

    update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,15 +7,15 @@ from sys import argv
 
 if __name__ == '__main__':
     user_page = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".
-        format(argv[1])).json()
+            "https://jsonplaceholder.typicode.com/users/{}".
+            format(argv[1])).json()
     todo_page = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".
-        format(argv[1])).json()
+            "https://jsonplaceholder.typicode.com/todos?userId={}".
+            format(argv[1])).json()
 
     with open("{}.csv".format(argv[1]), 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todo_page:
             csv_writer.writerow([int(argv[1]), user_page.get('username'),
-                                 task.get('completed'),
-                                 task.get('title')])
+                                task.get('completed'),
+                                task.get('title')])
