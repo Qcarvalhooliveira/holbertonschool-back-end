1ab12f5f399f36a330c439a4f5da3a10ea611183
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-22 18:52:28 +0200

    update

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,11 +7,11 @@ from sys import argv
 
 if __name__ == '__main__':
     user_page = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".
-        format(argv[1])).json()
+            "https://jsonplaceholder.typicode.com/users/{}".
+            format(argv[1])).json()
     todo_page = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".
-        format(argv[1])).json()
+            "https://jsonplaceholder.typicode.com/todos?userId={}".
+            format(argv[1])).json()
     username = user_page.get("username")
     user_id = argv[1]
 
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         task_dict["completed"] = task.get('completed')
         task_dict["username"] = username
         tasks.append(task_dict)
-    todos = {}
-    todos[user_id] = tasks
-    with open("{}.json".format(user_id), 'w') as jsonfile:
-        json.dump(todos, jsonfile)
+        todos = {}
+        todos[user_id] = tasks
+        with open("{}.json".format(user_id), 'w') as jsonfile:
+            json.dump(todos, jsonfile)
