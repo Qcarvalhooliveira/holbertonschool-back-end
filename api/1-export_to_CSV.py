a95532ba1afe301d063be8dcf3061172e7c1e58d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-22 12:29:14 +0200

    update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-   his/her TODO list progress."""
+"""Extends the 0-gather_data_from_an_API to export data in CSV format"""
 
 import csv
 import requests
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
+    user_info = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".
+        format(argv[1])).json()
+    user_tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".
+        format(argv[1])).json()
 
-    USER_ID = sys.argv[1]
-    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                             format(USER_ID)).json()
-
-    EMPLOYEE_NAME = user_info['username']
-
-    user_tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                              .format(USER_ID)).json()
-
-    with open(USER_ID + '.csv', 'w', newline='') as csv_file:
-        write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    with open("{}.csv".format(argv[1]), 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in user_tasks:
-            write.writerow([USER_ID, EMPLOYEE_NAME,
-                            task['completed'], task['title']])
+            csv_writer.writerow([int(argv[1]), user_info.get('username'),
+                                 task.get('completed'),
+                                 task.get('title')])
